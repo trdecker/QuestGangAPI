1ff477f1668e3ef6b6b70339a2f3c0aa6734e69b
Mahonri <ray22004@byui.edu>
2023-11-27 12:54:48 -0700

    testUser model and controller added. :D

--- src/controllers/testUserController.js
+++ src/controllers/testUserController.js
@@ -1,14 +1,14 @@
 const testUser = require('../models/testUserModel');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const {conditions, userStatus} = require('../types');
 
 async function testSignup(req, res) {
     try {
-        // const salt = await bcrypt.genSalt(10);
-        // const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
         const newUser = {
             username: req.body.username,
-            password: req.body.password
+            password: hashedPassword //req.body.password
         };
         const user = await testUser.createUser(newUser);
         res.status(200).send(user);
